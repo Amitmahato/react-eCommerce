be67b957b7dc68cd3bec8b384efd1367ea45dd1d
AmitMahato643 <amit643.mahato@gmail.com>
2021-07-15 22:09:40 +0545

    use antd Badge component for showing the number of items  in the cart

--- src/components/cart-icon/index.tsx
+++ src/components/cart-icon/index.tsx
@@ -3,6 +3,7 @@ import ShoppingBag from "../../assets/shopping-bag.png";
 import styled from "styled-components";
 import { useContext } from "react";
 import { CartContext } from "../../context/Cart-context";
+import { Badge } from "antd";
 
 const CartWrapper = styled.div`
   margin-left: auto;
@@ -10,24 +11,20 @@ const CartWrapper = styled.div`
   & img {
     width: 30px;
   }
-
-  & .cart-item-count {
-    margin-left: -10px;
-    background-color: black;
-    color: white;
-    padding: 0 4px;
-    border-radius: 50%;
-  }
 `;
 
 const CartIcon = () => {
   const { itemCount } = useContext(CartContext);
   return (
     <CartWrapper>
-      <img src={ShoppingBag} alt="shopping bag" />
-      {itemCount > 0 ? (
-        <span className="cart-item-count">{itemCount}</span>
-      ) : null}
+      <Badge
+        count={itemCount}
+        overflowCount={99}
+        offset={[-5, 24]}
+        size="small"
+      >
+        <img src={ShoppingBag} alt="shopping bag" />
+      </Badge>
     </CartWrapper>
   );
 };
